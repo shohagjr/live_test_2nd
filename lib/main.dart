2600b1ecc19f9ff1a38b105c2d75c2bbd62ed816
Shohag Joarder <68264567+shohagjr@users.noreply.github.com>
2024-11-15 22:48:25 +0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BloodDonationPage(),
+      home: NeedBlood(),
     );
   }
 }
 
-class BloodDonationPage extends StatelessWidget {
+class NeedBlood extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
